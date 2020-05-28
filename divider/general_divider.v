c1a08ff053eb8a99342c0a8ef66ec3d835ac0c10
Jordan Hong <jordanhong89@gmail.com>
2020-05-28 18:46:59 -0400

    Tested generic divider

--- divider/general_divider.v
+++ divider/general_divider.v
@@ -6,8 +6,8 @@ module general_divider #(
                         input clk, 
                         input [(WIDTH_A-1):0] A,
                         input [(WIDTH_B-1):0] B,
-                        output [(WIDTH_B-1):0] Q, 
-                        output [(WIDTH_A-1):0] R,
+                        output [(WIDTH_A-1):0] Q, 
+                        output [(WIDTH_B-1):0] R,
                         output reg done
     );
 
@@ -15,7 +15,7 @@ module general_divider #(
     // Declare Parameters
     //parameter WIDTH_A = 4;
     //parameter WIDTH_B = 4;
-    parameter n = WIDTH_A + WIDTH_B;
+    parameter n = 2*WIDTH_A;
 
     // Declare storing reg for R-A concat
     reg [(n-1):0] ra;
@@ -23,21 +23,21 @@ module general_divider #(
         
 
     initial begin
-        ra = {{WIDTH_B{1'b0}}, A};
+        ra = {{WIDTH_A{1'b0}}, A};
         c=0;
         done = 0;
         end
         
         
-    assign Q = ra[(WIDTH_A-1):0];
-    assign R = ra[(n-1):WIDTH_A];
+    assign Q = ra[0+:WIDTH_A];
+    assign R = ra[WIDTH_A+:WIDTH_B];
         
     always @ (posedge clk) begin
       
             if (c<WIDTH_A) begin
                 c<=c+1;
                 // if (R>=B), then R = R-b, and store q into the lower end of ra;
-                if ( ra[ (n-2):(WIDTH_A-1)] >= B)  ra <= ( (ra<<1) - {B, {WIDTH_A{1'b0}}}) | ( { {(n-1){1'b0}}, 1'b1} );
+                if ( ra[ (WIDTH_A-1) +:WIDTH_A] >= B )  ra <= ( (ra<<1) - { B, {WIDTH_A{1'b0}} } ) | ( { {(n-1){1'b0}}, 1'b1} );
                 else ra <= (ra<<1) | ({n{1'b0}}); // the or operation is redundant, just in case
             end
             
--- divider/testbench.v
+++ divider/testbench.v
@@ -2,10 +2,12 @@
 
 module testbench();
     parameter T =10;
-    reg [3:0] A= 4'b1111;
-    reg [3:0] B= 4'b0011;
-    wire [3:0] Q_gen;
-    wire [3:0] R_gen;
+    parameter wA=8;
+    parameter wB=4;
+    reg [(wA-1):0] A= 8'h8c;
+    reg [(wB-1):0] B= 4'b1001;
+    wire [(wA-1):0] Q_gen;
+    wire [(wB-1):0] R_gen;
     //wire [3:0] Q_sim, R_sim;
     wire DONE_gen;
     //wire DONE_sim;
@@ -15,7 +17,7 @@ module testbench();
     
     always #(T/2) clk = ~clk;
     
-    general_divider #(.WIDTH_A(4), .WIDTH_B(4)) SD (clk, A, B, Q_gen, R_gen, DONE_gen);
+    general_divider #(.WIDTH_A(wA), .WIDTH_B(wB)) SD (clk, A, B, Q_gen, R_gen, DONE_gen);
     //simple_divider SSD (clk, A, B, Q_sim, R_sim, DONE_sim);
 
 
